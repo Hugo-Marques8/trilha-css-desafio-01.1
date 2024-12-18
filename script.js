e295ff3d257e7a1275bb02ce9df5811ed5a5d3aa
Hugo <hugomarques888@outlook.com>
2024-12-18 20:31:20 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,4 @@
+document.getElementById("buguer").addEventListener("click", function () {
+  const menu = document.getElementById("menu");
+  menu.style.display = menu.style.display === "flex" ? "none" : "flex";
+});
